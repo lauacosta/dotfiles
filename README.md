191521b197d8fbef9be2923a97187fd61b47d397
lautaro <acostaquintalautaro@gmail.com>
2024-03-14 14:30:27 -0300

    Probando2

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# dotfiles
+Este repositorio contiene las aplicaciones y la configuracion que uso en mi maquina. Utilizo [`chezmoi`](https://github.com/twpayne/chezmoi). Hasta ahora esta configurada unicamente para Ubuntu.
+
+## Instalación:
+
+    export GITHUB_USERNAME=windrnr
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
---
+++ dot_config/awesome/rc.lua
@@ -0,0 +1,54 @@
+pcall(require, "luarocks.loader")
+
+local awful = require("awful")
+require("awful.autofocus")
+local beautiful = require("beautiful")
+local naughty = require("naughty")
+require("awful.hotkeys_popup.keys")
+
+if awesome.startup_errors then
+	naughty.notify({
+		preset = naughty.config.presets.critical,
+		title = "Oops, there were errors during startup!",
+		text = awesome.startup_errors,
+	})
+end
+
+do
+	local in_error = false
+	awesome.connect_signal("debug::error", function(err)
+		-- Make sure we don't go into an endless error loop
+		if in_error then
+			return
+		end
+		in_error = true
+
+		naughty.notify({
+			preset = naughty.config.presets.critical,
+			title = "Oops, an error happened!",
+			text = tostring(err),
+		})
+		in_error = false
+	end)
+end
+
+-- BASIC CONFIG
+beautiful.init("/home/lautaro/.config/awesome/theme.lua")
+terminal = "wezterm"
+editor = os.getenv("EDITOR") or "vim"
+editor_cmd = terminal .. " -e " .. editor
+modkey = "Mod4"
+
+awful.layout.layouts = {
+	awful.layout.suit.floating,
+	awful.layout.suit.tile,
+	awful.layout.suit.tile.left,
+	awful.layout.suit.tile.bottom,
+	awful.layout.suit.tile.top,
+}
+
+-- Load custom file configs
+require("wibar")
+require("keybindings")
+require("rules")
+require("signals")
