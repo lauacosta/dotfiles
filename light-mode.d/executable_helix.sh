b7a9da06d977dae26cfe308deb8112b38c72d227
Lautaro Acosta <acostaquintalautaro@gmail.com>
2025-10-22 19:31:13 -0300

    Cloud port

--- light-mode.d/executable_helix.sh
+++ light-mode.d/executable_helix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ln -sf ~/.config/helix/runtime/themes/light_theme.toml ~/.config/helix/runtime/themes/current_theme.toml
+ln -sf ~/.config/helix/runtime/themes/cloud.toml ~/.config/helix/runtime/themes/current_theme.toml
 
 
